c6f96d1dbd292fdcd2407e3963f03674244395fb
github-action-benchmark <github@users.noreply.github.com>
2026-01-07 16:26:34 +0000

    add Brillig Bytecode Size (customSmallerIsBetter) benchmark result for b7c6caab11e1d9da6a4ff4e61098bcba399a45cc

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767803193619,
+  "lastUpdate": 1767803194465,
   "repoUrl": "https://github.com/noir-lang/keccak256",
   "entries": {
     "ACIR Opcodes": [
@@ -90,6 +90,52 @@ window.BENCHMARK_DATA = {
             "name": "test_keccak256_256.json/main",
             "value": 51234,
             "unit": "circuit_size"
+          }
+        ]
+      }
+    ],
+    "Brillig Bytecode Size": [
+      {
+        "commit": {
+          "author": {
+            "email": "15848336+TomAFrench@users.noreply.github.com",
+            "name": "Tom French",
+            "username": "TomAFrench"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b7c6caab11e1d9da6a4ff4e61098bcba399a45cc",
+          "message": "chore: update actions permissions (#9)",
+          "timestamp": "2026-01-07T16:26:16Z",
+          "tree_id": "7950faa209d2d7d60e5a6be21c4d4919f13fa394",
+          "url": "https://github.com/noir-lang/keccak256/commit/b7c6caab11e1d9da6a4ff4e61098bcba399a45cc"
+        },
+        "date": 1767803194450,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "test_keccak256_1",
+            "value": 458,
+            "unit": "opcodes"
+          },
+          {
+            "name": "test_keccak256_100",
+            "value": 564,
+            "unit": "opcodes"
+          },
+          {
+            "name": "test_keccak256_135",
+            "value": 599,
+            "unit": "opcodes"
+          },
+          {
+            "name": "test_keccak256_256",
+            "value": 721,
+            "unit": "opcodes"
           }
         ]
       }
