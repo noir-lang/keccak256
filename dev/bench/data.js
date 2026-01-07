1d25c4596013ebad3bca1b32f506796bd0c6d6e9
github-action-benchmark <github@users.noreply.github.com>
2026-01-07 16:26:33 +0000

    add ACIR Opcodes (customSmallerIsBetter) benchmark result for b7c6caab11e1d9da6a4ff4e61098bcba399a45cc

---
+++ dev/bench/data.js
@@ -0,0 +1,52 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1767803193002,
+  "repoUrl": "https://github.com/noir-lang/keccak256",
+  "entries": {
+    "ACIR Opcodes": [
+      {
+        "commit": {
+          "author": {
+            "email": "15848336+TomAFrench@users.noreply.github.com",
+            "name": "Tom French",
+            "username": "TomAFrench"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b7c6caab11e1d9da6a4ff4e61098bcba399a45cc",
+          "message": "chore: update actions permissions (#9)",
+          "timestamp": "2026-01-07T16:26:16Z",
+          "tree_id": "7950faa209d2d7d60e5a6be21c4d4919f13fa394",
+          "url": "https://github.com/noir-lang/keccak256/commit/b7c6caab11e1d9da6a4ff4e61098bcba399a45cc"
+        },
+        "date": 1767803192776,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "test_keccak256_1.json/main",
+            "value": 1305,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "test_keccak256_100.json/main",
+            "value": 1899,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "test_keccak256_135.json/main",
+            "value": 2109,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "test_keccak256_256.json/main",
+            "value": 3992,
+            "unit": "acir_opcodes"
+          }
+        ]
+      }
+    ]
+  }
+}
