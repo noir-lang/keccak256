64522d5820cc152feb37cb5c336a2c24428351c9
github-action-benchmark <github@users.noreply.github.com>
2026-01-27 17:16:21 +0000

    add ACIR Opcodes (customSmallerIsBetter) benchmark result for 1c023d9e6a44b48d866058121d0d3581b795918a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1767829079917,
+  "lastUpdate": 1769534181012,
   "repoUrl": "https://github.com/noir-lang/keccak256",
   "entries": {
     "ACIR Opcodes": [
@@ -219,6 +219,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "test_keccak256_256.json/main",
             "value": 3992,
+            "unit": "acir_opcodes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "15848336+TomAFrench@users.noreply.github.com",
+            "name": "Tom French",
+            "username": "TomAFrench"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1c023d9e6a44b48d866058121d0d3581b795918a",
+          "message": "feat: unroll loops to improve brillig runtime performance (#14)",
+          "timestamp": "2026-01-27T17:16:03Z",
+          "tree_id": "412051bf3bbaba94405eaf6e80b0e6a0f712de63",
+          "url": "https://github.com/noir-lang/keccak256/commit/1c023d9e6a44b48d866058121d0d3581b795918a"
+        },
+        "date": 1769534180705,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "test_keccak256_1.json/main",
+            "value": 885,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "test_keccak256_100.json/main",
+            "value": 1479,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "test_keccak256_135.json/main",
+            "value": 1689,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "test_keccak256_256.json/main",
+            "value": 3147,
             "unit": "acir_opcodes"
           }
         ]
